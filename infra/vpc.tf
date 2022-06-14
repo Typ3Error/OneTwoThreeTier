6e13e44242ca8dac5b11d8b63278fbf6eaf18459
hello697 <hello@tycodes.com>
2022-06-14 19:22:36 -0400

    Add internet gw to pub route table, add subnet association for pub rt

--- infra/vpc.tf
+++ infra/vpc.tf
@@ -13,58 +13,83 @@ resource "aws_vpc" "tetra_VPC" {
 resource "aws_internet_gateway" "tetra_gw" {}
 
 # Configure Subnets
-resource "aws_subnet" "tetra_subnet_one" {
+resource "aws_subnet" "tetra_public_sub_one" {
   vpc_id     = aws_vpc.tetra_VPC.id
   cidr_block = "10.0.1.0/24"
 
   tags = {
-    Name = "tetra_sub_one"
+    Name = "tetra_public_sub_one"
   }
 }
 
-resource "aws_subnet" "tetra_subnet_two" {
+resource "aws_subnet" "tetra_public_sub_two" {
   vpc_id     = aws_vpc.tetra_VPC.id
   cidr_block = "10.0.2.0/24"
 
   tags = {
-    Name = "tetra_sub_two"
+    Name = "tetra_public_sub_two"
   }
 }
 
-resource "aws_subnet" "tetra_subnet_three" {
+resource "aws_subnet" "tetra_private_sub_one" {
   vpc_id     = aws_vpc.tetra_VPC.id
   cidr_block = "10.0.3.0/24"
 
   tags = {
-    Name = "tetra_sub_three"
+    Name = "tetra_private_sub_one"
   }
 }
 
-resource "aws_subnet" "tetra_subnet_four" {
+resource "aws_subnet" "tetra_private_sub_two" {
   vpc_id     = aws_vpc.tetra_VPC.id
   cidr_block = "10.0.4.0/24"
 
   tags = {
-    Name = "tetra_sub_four"
+    Name = "tetra_private_sub_two"
   }
 }
 
-resource "aws_route_table" "tetra_rt_one" {
+resource "aws_route_table" "tetra_public_rt" {
   vpc_id = aws_vpc.tetra_VPC.id
 
   route = []
 
   tags = {
-    Name = "tetra_rt_one"
+    Name = "tetra_public_rt"
   }
 }
 
-resource "aws_route_table" "tetra_rt_two" {
+resource "aws_route" "tetra_pub_gw" {
+  route_table_id         = aws_route_table.tetra_public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.tetra_gw.id
+
+
+}
+
+resource "aws_route_table" "tetra_private_rt" {
   vpc_id = aws_vpc.tetra_VPC.id
 
   route = []
 
   tags = {
-    Name = "tetra_rt_two"
+    Name = "tetra_private_rt"
   }
 }
+
+resource "aws_route_table_association" "tetra_pub_association_one" {
+  subnet_id      = aws_subnet.tetra_public_sub_one.id
+  route_table_id = aws_route_table.tetra_public_rt.id
+}
+
+resource "aws_route_table_association" "tetra_pub_association_two" {
+  subnet_id      = aws_subnet.tetra_public_sub_two.id
+  route_table_id = aws_route_table.tetra_public_rt.id
+}
+
+
+
+
+
+
+
