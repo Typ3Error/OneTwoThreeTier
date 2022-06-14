6ac5e56fbbe90eb6c5d249b78431d94c755f3cf1
hello697 <hello@tycodes.com>
2022-06-14 18:54:19 -0400

    Added 4 subnets

--- infra/vpc.tf
+++ infra/vpc.tf
@@ -11,3 +11,42 @@ resource "aws_vpc" "tetra_VPC" {
 
 # Create Internet Gateway
 resource "aws_internet_gateway" "tetra_gw" {}
+
+# Configure Subnets
+resource "aws_subnet" "tetra_subnet_one" {
+  vpc_id     = aws_vps.tetra_VPC.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "tetra_sub_one"
+  }
+}
+
+resource "aws_subnet" "tetra_subnet_twp" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "tetra_sub_two"
+  }
+}
+
+resource "aws_subnet" "tetra_subnet_three" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.3.0/24"
+
+  tags = {
+    Name = "tetra_sub_three"
+  }
+}
+
+resource "aws_subnet" "tetra_subnet_four" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.4.0/24"
+
+  tags = {
+    Name = "tetra_sub_four"
+  }
+}
+
+
